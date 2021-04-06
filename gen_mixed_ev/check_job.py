4b7f10f14a6a11260969b71d6765a99a64e777f2
Yasuki Tachibana <yasukitachibana@Yasuki-iMac.local>
2021-04-06 17:18:20 +0900

    update check_job

---
+++ gen_mixed_ev/check_job.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import time
+
+def GheckJob(id, tag):
+
+  n_run = NumberOfRUnningJobs(id,tag)
+  while n_run > 0:
+    #time.sleep(120)
+    time.sleep(10)
+    n_run = NumberOfRunningJobs(id,tag)
+    print( n_ev, '\b-jobs are running now.')
+
+  return 1
+  
+ 
+ 
+def NumberOfRunningJobs(id,tag):
+  command = 'squeue -u {} | grep {} | wc'.format(id,tag)
+  #cmd = 'ls | wc'
+  out = subprocess.Popen(
+      cmd, stdout=subprocess.PIPE,
+      shell=True).communicate()[0]
+  return int(str(out).split()[1])
+
+def main():
+
+  
+  import argparse
+  parser = argparse.ArgumentParser()
+    
+  parser.add_argument("--u", type=str, default='go5410')
+  parser.add_argument("--tag", type=str, default='jet_analysis')
+  
+  args = parser.parse_args()
+  
+  n_run = NumberOfRunningJobs(args.u, args.tag)
+  GheckJob(id, tag)
+  print(n_run)
+
+
+if __name__ == '__main__':
+  main()
