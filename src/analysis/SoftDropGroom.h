fe6882685b32a8bc4ca8f61e84c93d911c4aa8d0
yasukitachibana <yasuki.tachibana@gmail.com>
2021-05-29 14:38:50 +0900

    update soft drop

--- src/analysis/SoftDropGroom.h
+++ src/analysis/SoftDropGroom.h
@@ -6,46 +6,84 @@
 #include "negative_recombiner.h"
 #include <iostream>
 
-class SoftDropGroom : public AnalysisModuleStandard {
+class SDAdditionalCondition
+{
+public:
+  virtual ~SDAdditionalCondition() {}
+  virtual bool Trigger(double val) { return false; }
+  virtual bool Trigger(double val1, double val2) { return false; }
+  virtual bool Trigger(double val1, double val2, double val3) { return false; }
+};
+
+class SDNoCondition : public SDAdditionalCondition
+{
+public:
+  SDNoCondition() {}
+  ~SDNoCondition() {}
+  bool Trigger(double val) { return true; }
+  bool Trigger(double val1, double val2) { return true; }
+  bool Trigger(double val1, double val2, double val3) { return true; }
+};
+
+class SDDeltaRCut : public SDAdditionalCondition
+{
+public:
+  SDDeltaRCut(double delta_r_min) : deltaRMin(delta_r_min) {}
+  ~SDDeltaRCut() {}
+  bool Trigger(double val)
+  {
+    if (val > deltaRMin)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
 private:
-  
+  const double deltaRMin;
+};
+
+class SoftDropGroom : public AnalysisModuleStandard
+{
+private:
   // Allows the registration of the module so that it is available to be used by the Analysis framework.
   static RegisterAnalysisModule<SoftDropGroom> reg;
   const std::string name;
-  
-  
-  void OneEventAnalysis(std::vector<std::shared_ptr<Particle>> particle_list);
 
+  void OneEventAnalysis(std::vector<std::shared_ptr<Particle>> particle_list);
 
   void CombineHist(int iv, int ir, int ijp, int ijr, int ipp, int ipr, int ip);
 
   int ReadOptionParametersFromXML();
   void ShowParamsSetting();
-  
-  std::string GetParamsTag( int i );
-  std::string GetParamsTag( std::array<int, 2> i );
-  std::string GetParamsTag( int i_beta, int i_zCut );
-  std::string GetParamsTag( double beta_sd, double z_cut_sd );
 
-  int GetParamIndex( int i_beta, int i_zCut );
-  int GetParamIndex( std::array<int, 2> i );
-  std::array<int, 2> GetParamIndex( int i);
-  
-  
+  std::string GetParamsTag(int i);
+  std::string GetParamsTag(std::array<int, 2> i);
+  std::string GetParamsTag(int i_beta, int i_zCut);
+  std::string GetParamsTag(double beta_sd, double z_cut_sd);
+
+  int GetParamIndex(int i_beta, int i_zCut);
+  int GetParamIndex(std::array<int, 2> i);
+  std::array<int, 2> GetParamIndex(int i);
+
   std::vector<double> beta;
   std::vector<double> zCut;
-  
+
   int i_zG;
   int i_thetaG;
-  
+  int i_rG;
+
   int ui;
-  
+
+  std::unique_ptr<SDAdditionalCondition> additional_cond_ptr;
+
 public:
   SoftDropGroom(std::string name_in = "SoftDropGroom");
   ~SoftDropGroom();
-  std::string Name(){return name;}
-  
-  
+  std::string Name() { return name; }
 };
 
 #endif // SoftDropGroom_H
