4bc89f3ea95a6771e76951f97c866246ae850e3a
yasukitachibana <yasuki.tachibana@gmail.com>
2021-11-16 10:03:41 +0900

    for test

--- src/analysis/JetShape.cc
+++ src/analysis/JetShape.cc
@@ -50,7 +50,6 @@ void JetShape::SetObservable(fastjet::PseudoJet jet,
                              std::vector<std::shared_ptr<Particle>> particle_list,
                              int ir, int ijp, int ijr)
 {
-
   //--------------------------------------------------------------------------------------------------
   for (int iv = 0; iv < variables.size(); iv++)
   {
--- src/framework/AnalysisModuleStandard.cc
+++ src/framework/AnalysisModuleStandard.cc
@@ -1,86 +1,113 @@
 #include "AnalysisModuleStandard.h"
 
-
-
-
-void AnalysisModuleStandard::EventEndMark
- (std::vector<std::shared_ptr<Particle>> &particle_list, int &event_num)
+void AnalysisModuleStandard::EventEndMark(std::vector<std::shared_ptr<Particle>> &particle_list, int &event_num)
 {
-  
-  if(event_num%2500==0){
-    std::cout
-    << "Event" << event_num
-    <<" ("<<std::to_string(getMemoryUsage())<<"MB) "
-    << std::flush;
-    
 
-    
-    if( ((event_num/2500)+1)%5==0 ){
-      std::cout<<std::endl;
+  //================================================
+  if (event_num == 0)
+  {
+    std::string jet_infos_filename = "jet_infos.txt";
+    std::cout
+        << "[AnalysisModuleStandard, EventEndMark] Creating Output File:"
+        << jet_infos_filename
+        << std::endl;
+    jet_infos.open(jet_infos_filename.c_str(),
+                   std::ios::out | std::ios::binary | std::ios::trunc);
+    jet_infos
+        << "# current_event" << " "
+        << "n_jet" << " "
+        << "j.perp()" << " "
+        << "j.rapidity()" << " "
+        << "j.phi()" << " "
+        << "j.eta()" << "\n";
+  }
+  current_event = event_num;
+  //================================================
+
+  if (event_num % 2500 == 0)
+  {
+    std::cout
+        << "Event" << event_num
+        << " (" << std::to_string(getMemoryUsage()) << "MB) "
+        << std::flush;
+
+    if (((event_num / 2500) + 1) % 5 == 0)
+    {
+      std::cout << std::endl;
     }
   }
 
-//  auto pf =  particle_list.back();
-//
-//  std::cout
-//  << "Event:" << event_num
-//  << ", particle number: " << particle_list.size()
-//  << ", stat:" << pf->pstat()
-//  << ", pid:"<< pf->pid()
-//  << ", plabel:" <<pf->plabel()
-//  << ", e:" <<pf->e()
-//  << ", px:" <<pf->px()
-//  << ", py:" <<pf->py()
-//  << ", eta:"<< pf->eta()
-//  << ", phi:" <<pf->phi_std()
-//  << std::endl;
+  //  auto pf =  particle_list.back();
+  //
+  //  std::cout
+  //  << "Event:" << event_num
+  //  << ", particle number: " << particle_list.size()
+  //  << ", stat:" << pf->pstat()
+  //  << ", pid:"<< pf->pid()
+  //  << ", plabel:" <<pf->plabel()
+  //  << ", e:" <<pf->e()
+  //  << ", px:" <<pf->px()
+  //  << ", py:" <<pf->py()
+  //  << ", eta:"<< pf->eta()
+  //  << ", phi:" <<pf->phi_std()
+  //  << std::endl;
 
-  
-  
-  for( auto hist: hist_list){
+  for (auto hist : hist_list)
+  {
     hist->EventCount();
   }
-  
-  
+
   OneEventAnalysis(particle_list);
   particle_list.clear();
   particle_list.shrink_to_fit();
   event_num++;
-  
 }
 
+void AnalysisModuleStandard::OneEventAnalysis(std::vector<std::shared_ptr<Particle>> particle_list)
+{
 
-void AnalysisModuleStandard::OneEventAnalysis(std::vector<std::shared_ptr<Particle>> particle_list){
-  
-  for(int ir = 0; ir < jetR.size(); ir++ ){
-    
+  for (int ir = 0; ir < jetR.size(); ir++)
+  {
+
     double r_cone = jetR[ir];
     int n_jet = 0;
     // Get Jets Reconstructed with Jet Cone Size = r_cone
-    std::vector <fastjet::PseudoJet> jets = reco_ptr->JetReco( r_cone, particle_list);
-    for(auto j:jets){
-      
-      //std::cout << "jet#" << i_jet << std::endl;
-      
-      for( int ijp = 0; ijp < jetPtMin.size(); ijp++ ){
-        for( int ijr = 0; ijr < jetRapMin.size(); ijr++ ){
-          
-          if( JetTrigger(j, ir, ijp, ijr) ){
+    std::vector<fastjet::PseudoJet> jets = reco_ptr->JetReco(r_cone, particle_list);
+    for (auto j : jets)
+    {
 
-            SetObservable(j, particle_list, ir, ijp, ijr );
-            
-          }//trigger
-          
+      //std::cout << "jet#" << i_jet << std::endl;
+
+      for (int ijp = 0; ijp < jetPtMin.size(); ijp++)
+      {
+        for (int ijr = 0; ijr < jetRapMin.size(); ijr++)
+        {
+
+          if (JetTrigger(j, ir, ijp, ijr))
+          {
+
+            SetObservable(j, particle_list, ir, ijp, ijr);
+            //================================================
+            jet_infos
+                << current_event << " "
+                << n_jet << " "
+                << j.perp() << " "
+                << j.rapidity() << " "
+                << j.phi() << " "
+                << j.eta() << "\n";
+            //================================================
+
+          } //trigger
         }
       }
-            
+
       n_jet++;
-      if(n_jet == nJetEv ){
+      if (n_jet == nJetEv)
+      {
         break;
       }
 
-    }//jet
+    } //jet
 
-  }//jetR
-  
+  } //jetR
 }
