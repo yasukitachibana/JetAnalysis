ce8ca6ca7e19d7fb82cf2987c1da4cf5736d7a80
Yasuki Tachibana <yasukitachibana@Yasukis-iMac.local>
2022-05-12 00:28:18 +0900

    lates

--- config/ExtraPythonScripts/SD_ATLAS.py
+++ config/ExtraPythonScripts/SD_ATLAS.py
@@ -154,7 +154,7 @@ def  Make1DTablePt(main_results_dir, pt_rg_2d_data, rg_bin_finest, pt_bin_finest
     bin_list = []
     print('# Combine r bins', end='')       
     for l in range(k[0],k[1]):
-      r_slice = slice( l, raw_number , n_pt_bin )
+      r_slice = slice( l, raw_number+1 , n_pt_bin )
       data_2D = pt_rg_2d_data[r_slice,:]
       print(' [{:.2f}-{:.2f}] '.format(data_2D[0,4],data_2D[0,5]), end='')      
       data_1D = mdata.Reduc2to1D(data_2D,0)
