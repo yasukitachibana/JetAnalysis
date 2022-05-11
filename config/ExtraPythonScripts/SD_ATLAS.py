81dc6506d904fed8511e61892537447a95016222
Yasuki Tachibana <yasukitachibana@Yasukis-iMac.local>
2022-05-12 05:35:45 +0900

    lates

--- config/ExtraPythonScripts/SD_ATLAS.py
+++ config/ExtraPythonScripts/SD_ATLAS.py
@@ -123,7 +123,7 @@ def  Make1DTableRg(main_results_dir, pt_rg_2d_data, rg_bin_finest, pt_bin_finest
       print(' [{}-{}] '.format(int(data_2D[0,1]),int(data_2D[0,2])), end='')            
       data_1D = mdata.Reduc2to1D(data_2D,1)
       ptbin = data_2D[:,2] - data_2D[:,1]
-      #print(ptbin)
+      print(ptbin)
       data_list.append(data_1D)
       bin_list.append(ptbin)
 
