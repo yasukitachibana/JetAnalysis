af9022c2560a74fa379ab42335e70852b2436877
Yasuki Tachibana <yasukitachibana@Yasukis-iMac.local>
2023-02-17 00:56:26 +0900

    minor

--- src/main.cc
+++ src/main.cc
@@ -1,75 +1,92 @@
 #include <iostream>
 #include "JetAnalysis.h"
 
-//#######################################################
+// #######################################################
 bool run_code = false;
 std::string xml_filename;
 std::string input_dirname;
 std::string output_dirname;
-//#######################################################
+// #######################################################
 void Show();
-void CheckArg(int argc, char** argv);
-//#######################################################
-int main(int argc, char** argv)
+void CheckArg(int argc, char **argv);
+// #######################################################
+int main(int argc, char **argv)
 {
-  
-  int StartTime = time(NULL);
-  
-  //#######################################################
-  Show(); // Show the Code information
-  CheckArg(argc, argv); // Check Arguments (OK->run_code=true,NO->run_code=false)
-  //#######################################################
-  
-  //#######################################################
-  // Execution 
-  //#######################################################
-  if(run_code){ // Run Codes when Arguments are Good
-  //Jet Analysis
-  JetAnalysis ja(xml_filename,input_dirname,output_dirname);
-  ja.Init(); //Initialize Jet Analysis
-  ja.Exec(); //Start Jet Analysis  
-  }
-  //#######################################################
 
-  //#######################################################
-  // Finish Code
-  //#######################################################
+  int StartTime = time(NULL);
+
+  // #######################################################
+  Show();               // Show the Code information
+  CheckArg(argc, argv); // Check Arguments (OK->run_code=true,NO->run_code=false)
+  // #######################################################
+
+  // #######################################################
+  //  Execution
+  // #######################################################
+  if (run_code)
+  { // Run Codes when Arguments are Good
+    // Jet Analysis
+    JetAnalysis ja(xml_filename, input_dirname, output_dirname);
+    ja.Init(); // Initialize Jet Analysis
+    ja.Exec(); // Start Jet Analysis
+  }
+  // #######################################################
+
+  // #######################################################
+  //  Finish Code
+  // #######################################################
   std::cout << std::endl;
-  std::cout<<"#########################################################"<<std::endl;
+  std::cout << "#########################################################" << std::endl;
   int EndTime = time(NULL);
-  int Hour = (EndTime-StartTime)/3600;
-  int Minute = ((EndTime-StartTime)/60)-Hour*60;
-  int Second = (EndTime-StartTime)-Hour*60*60 - Minute*60;
-  std::cout<<"Programme run time = "<<Hour<<"::"<<Minute<<"::"<<Second<<std::endl;
-  std::cout<<"#########################################################"<<std::endl;
+  int Hour = (EndTime - StartTime) / 3600;
+  int Minute = ((EndTime - StartTime) / 60) - Hour * 60;
+  int Second = (EndTime - StartTime) - Hour * 60 * 60 - Minute * 60;
+  std::cout << "Programme run time = " << Hour << "::" << Minute << "::" << Second << std::endl;
+  std::cout << "#########################################################" << std::endl;
   std::cout << std::endl;
   return 0;
 }
 
-void CheckArg(int argc, char** argv){
-  std::cout <<  "=========================================================" << std::endl;
-  if( argc > 1 && !strcmp(argv[1],"test") ){
+void CheckArg(int argc, char **argv)
+{
+  std::cout << "=========================================================" << std::endl;
+  if (argc > 1 && !strcmp(argv[1], "test"))
+  {
     std::cout << "-->test mode<--" << std::endl;
     run_code = true;
-    xml_filename = "../config/test.xml";
-    input_dirname = "../test_data";
-    output_dirname = "./";
-  }else if(argc > 3){
+    if (argc > 2)
+    {
+      xml_filename = argv[2];
+      input_dirname = "../test_data";
+      output_dirname = "./";
+    }
+    else
+    {
+      xml_filename = "../config/test.xml";
+      input_dirname = "../test_data";
+      output_dirname = "./";
+    }
+  }
+  else if (argc > 3)
+  {
     std::cout << "-->run code<--" << std::endl;
     run_code = true;
     xml_filename = argv[1];
     input_dirname = argv[2];
     output_dirname = argv[3];
-  }else{
+  }
+  else
+  {
     run_code = false;
     std::cout << "Please Specify XML File, input directory and output directory" << std::endl;
     std::cout << ".Main [XML File] [input directory] [output directory]" << std::endl;
   }
-  std::cout <<  "=========================================================" << std::endl;
+  std::cout << "=========================================================" << std::endl;
 }
 
-void Show(){
-  std::cout<<"#########################################################"<<std::endl;
+void Show()
+{
+  std::cout << "#########################################################" << std::endl;
   std::cout << std::endl;
   std::cout << "---------------------" << std::endl;
   std::cout << "- Jet Analysis Code -" << std::endl;
@@ -77,4 +94,3 @@ void Show(){
   std::cout << "---------------------" << std::endl;
   std::cout << std::endl;
 }
-
