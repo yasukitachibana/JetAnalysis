08cd632b45191b6d2d8c483fdb2e82017899b61b
yasukitachibana <yasuki.tachibana@gmail.com>
2023-08-16 01:28:27 +0900

    test

--- src/analysis/JetSpec.h
+++ src/analysis/JetSpec.h
@@ -2,6 +2,7 @@
 #define JETSPEC_H
 
 #include "AnalysisModuleStandard.h"
+#include "negative_recombiner.h"
 #include <iostream>
 
 class JetSpec : public AnalysisModuleStandard
@@ -11,11 +12,14 @@ private:
   static RegisterAnalysisModule<JetSpec> reg;
   const std::string name;
 
-  void SetObservable(int i_tag_particle, fastjet::PseudoJet jet,
-                     std::vector<std::shared_ptr<Particle>> particle_list,
-                     int ir, int ijp, int ijr);
+  void OneEventAnalysis(std::vector<std::shared_ptr<Particle>> particle_list, int i_tag_particle);
+  // void SetObservable(int i_tag_particle, fastjet::PseudoJet jet,
+  //                    std::vector<std::shared_ptr<Particle>> particle_list,
+  //                    int ir, int ijp, int ijr);
 
   void CombineHist(int iv, int ir, int ijp, int ijr, int ipp, int ipr, int ip);
+
+  int ui;
 
 public:
   JetSpec(std::string name_in = "JetSpec");
